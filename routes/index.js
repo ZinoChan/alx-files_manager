1a7b57e2565fb11f3603615d8449711672711f9f
ZinoChan <devzinochan@gmail.com>
2024-02-10 22:13:36 +0100

    add auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,42 @@
-import Router from 'express'
+import Router from "express";
 
 class AppRoutes {
-  constructor(appController, userController) {
-	this.router = Router();
-	this.appController = appController;
-	this.userController = userController;
-	this.initializeRoutes();
+  constructor(
+    appController,
+    userController,
+    authController,
+    authMiddleware,
+    fileController
+  ) {
+    this.router = Router();
+    this.appController = appController;
+    this.userController = userController;
+    this.authController = authController;
+    this.authMiddleware = authMiddleware;
+    this.fileController = fileController;
+    this.initializeRoutes();
   }
 
   initializeRoutes() {
-	this.router.get('status', this.appController.getStatus);
-	this.router.get('stats', this.appController.getStats);
-	this.router.post('users', this.userController.register);
+    this.router.get("status", this.appController.getStatus);
+    this.router.get("stats", this.appController.getStats);
+    this.router.post("users", this.userController.register);
+    this.router.get(
+      "connect",
+      this.authMiddleware.authenticate,
+      this.authController.getConnect
+    );
+    this.router.get("disconnect", this.authController.getDisconnect);
+    this.router.get(
+      "users/me",
+      this.authMiddleware.authenticate,
+      this.userController.getMe
+    );
+    this.router.post(
+      "files",
+      this.authMiddleware.authenticate,
+      this.fileController.postUpload
+    );
   }
 }
 
