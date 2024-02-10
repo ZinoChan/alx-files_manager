fd8f7532be257441e20c9584a99d9c6f176091a3
ZinoChan <devzinochan@gmail.com>
2024-02-10 22:13:53 +0100

    minor change

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,28 +1,33 @@
-
 class AuthMiddleware {
-	constructor(db) {
-		this.db = db;
-	}
-	  static async authenticate(req, res, next) {
-	const token = req.headers.authorization;
-	if (!token || !token.startsWith('Basic ')) {
-	  return res.status(401).json({ message: 'Unauthorized' });
-	}
-	try {
-		const encoded = token.slice('Basic '.length);
-	  const decoded = Buffer.from(encoded, 'base64').toString('utf-8');
-	  const [email, password] = decoded.split(':');
-	  const hashedPassword = createHash('sha1').update(password).digest('hex');
+  constructor(db) {
+    this.db = db;
+  }
+  static async authenticate(req, res, next) {
+    const token = req.headers.authorization;
+    if (!token || !token.startsWith("Basic ")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    try {
+      const encoded = token.slice("Basic ".length);
+      const decoded = Buffer.from(encoded, "base64").toString("utf-8");
+      const [email, password] = decoded.split(":");
+      const hashedPassword = createHash("sha1").update(password).digest("hex");
 
-	  const user = await this.db.usersCollection.findOne({ email, password: hashedPassword });
-	  if (!user) {
-		return res.status(401).json({ message: 'Unauthorized' });
-	  }
+      const user = await this.db.usersCollection.findOne({
+        email,
+        password: hashedPassword,
+      });
+      if (!user) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
 
-	  req.user = user;
-	  next();
-	} catch (error) {
-	  return res.status(401).json({ message: 'Unauthorized' });
-	}
+      req.user = user;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
   }
 }
+
+
+export default AuthMiddleware;
