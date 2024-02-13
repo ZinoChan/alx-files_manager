e7a6857fe514f16ab2cbf9fd9c214ce8efc22b13
ZinoChan <devzinochan@gmail.com>
2024-02-13 19:45:19 +0100

    fix

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,7 @@ import redis from '../utils/redis';
 import dbClient from '../utils/db';
 
 class UsersController {
-  async postNew(req, res) {
+  static async postNew(req, res) {
     const { email, password } = req.body;
 
     if (!email) return res.status(400).json({ error: 'Missing email' });
